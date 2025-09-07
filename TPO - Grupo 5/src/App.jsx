cc3fc3119c541bd1583bebae65b7a68c4b940160
Peperina dev <peperinadev@gmail.com>
2025-09-07 20:37:40 -0300

    creacion de cart-context y ajuste de logica para que el carrito funcione en el principal

--- TPO - Grupo 5/src/App.jsx
+++ TPO - Grupo 5/src/App.jsx
@@ -3,19 +3,24 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './app/Page'
 import Products from './app/products/Page'
-import ProductDetails  from './app/products/product/Page'
+import ProductDetails from './app/products/product/Page'
+
+// 👇 importá el provider
+import { CartProvider } from './contexts/cart-contexts'
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
-        </Routes>
-      </div>
-    </Router>
+    <CartProvider> {/* 👈 envolver toda la app */}
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
   )
 }
 
--- TPO - Grupo 5/src/components/Header.jsx
+++ TPO - Grupo 5/src/components/Header.jsx
@@ -1,15 +1,18 @@
 import "../styles/Header.css"
 import { Link } from 'react-router-dom';
+import { useCart } from "../contexts/cart-contexts"
 
 export function Header() {
+  const { getTotalItems } = useCart()
+  const totalItems = getTotalItems()
+
   return (
     <header className="header">
       <div className="header-container">
         <div className="header-content">
-
+          
           <div className="logo">
-             <Link to="/"><h1>TiendaGrupo5</h1></Link> 
-            
+            <Link to="/"><h1>TiendaGrupo5</h1></Link> 
           </div>
 
           <nav className="nav">
@@ -18,6 +21,7 @@ export function Header() {
             <Link to="/categories">Categorías</Link>
           </nav>
 
+          {/* Actions */}
           <div className="actions">
             <button className="btn btn-hidden-sm">
               <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -38,7 +42,7 @@ export function Header() {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4m0 0L7 13m0 0l-1.5 6M7 13l-1.5 6m0 0h9M17 13v6a2 2 0 01-2 2H9a2 2 0 01-2-2v-6"
                 />
               </svg>
-              <span className="cart-badge">3</span>
+              {totalItems > 0 && <span className="cart-badge">{totalItems}</span>}
             </button>
             <button className="btn btn-hidden-sm">
               <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
